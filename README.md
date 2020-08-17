931cdf15913f21d96e4296ecc53aa2a1eb7529c1
JoannaTanveer <joanna.tanveer@gmail.com>
2020-08-16 18:41:10 -0700

    working, needs editing

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# README_generator
+#jo
+
+
+    (https://img.shields.io/github/last-commit/jo/jo?style=plastic)
+    
+    This is an application that jo. The purpose of this application is to solve jo. 
+    
+    ###Demo 
+    (jo)
+    [jo](jo)
+    ##Table of Contents
+        
+            *[Technologies](#technologies)
+            *[Installation](#installation)
+            *[Usage](#usage)
+            *[Credits](#credits)
+            *[Contributing](#contributing)
+            *[License](#license)
+    
+    ###Technologies
+    
+  
+    The technologies utilized in this application are as follows: jo, jo, and jo. 
+        jo was used because jo. (https://img.shields.io/npm/l/jo?style=plastic)
+        jo was used because jo. 
+        jo was used because jo. 
+    
+    ###Installation
+    
+        To run this application successfully follow these steps:
+        1-Install undefined to your local drive vis terminal/GitBash. This will install the package.json file which contains all the dependencies for this application. 
+        2-Verify that node is installed in your computer by typing 'node' in the terminal/GitBash. If it returns
+                Welcome to Node.js v12.14.1. (or higher)
+                Type ".help" for more information.
+            Node.js is installed in your computer. If nothing happens or there is an error, visit [Node.js](https://nodejs.org/) and install the LTS version.
+        3-
+    
+    ###Usage
+    
+        The purpose of this program is to solve jo, as mentioned above. Let's look a little further at how that can be accomplished.
+        *jo -  undefined
+        *jo -  undefined
+    
+    ###Credits
+    
+        The Dev community prides itself in the open source culture that it celebrates and maintains. This application couldn't have been made possible without the help of
+        *jo for their undefined. jo
+        *jo for her undefined. jo
+        *jo for her undefined. jo 
+    
+        ####Thank You!
+    
+    ###Contributing
+    
+    ###License
+            Licensed under the [MIT](license) license. (https://img.shields.io/github/license/jo/jo?style=plastic)
+    
--- index.js
+++ index.js
@@ -1,1 +1,235 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const questions =  [
+    {
+     type: 'input',
+     message: "what is the title of your application?",
+     name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub user name (exactly as it appears)?',
+        name: 'userName'
+    },
+    {
+        type: 'input',
+        message: 'What is your repositry name (exactly as it appears)',
+        name: 'repoName'
+    
+    },
+    {
+        type: 'input',
+        message: 'Please input your live applications link. (exactly as it appears)',
+        name: 'liveLink'
+    },
+    {
+        type: 'input',
+        message: 'Please input the link to your repository',
+        name: 'respositoryLink'
+    },
+    {
+        type: 'input',
+        message: 'Please provide a short description of the application. (2-3 sentances)',
+        name: 'shortDescription'
+    },
+    {
+        type: 'input',
+        message: 'In 1 brief sentance state the problem the application is trying to solve.',
+        name: 'problem'
+    },
+    {
+        type: 'input', 
+        message: 'Please input the relative path for a screenshot (.png) or .gif',
+        name: 'screenshot'
+    },
+    {
+        type: 'input',
+        message: 'Which npm package was utilized?',
+        name: 'npmPackage'
+    },
+    {
+        type: 'input',
+        message: 'Why did you utilize that package? What benefit did it provide?',
+        name: 'tech1Why'
+    },
+    {
+        type: 'input',
+        message: 'What other technology did you utilize? (Can be another package or any other technology)',
+        name: 'technology2'
+    },
+    {
+        type: 'input',
+        message: 'Why did you utilize that technology/package? What benefit did it provide?',
+        name: 'tech2Why'
+    },
+    {
+        type: 'input',
+        message: 'What other technology did you utilize? (Can be another package or any other technology)',
+        name: 'technology3'
+    },
+    {
+        type: 'input',
+        message: 'Why did you utilize that technology/package? What benefit did it provide?',
+        name: 'tech3Why'
+    },
+    {
+        type: 'input',
+        message: 'What is the first functionality you would like to highligh? (it should show how you are solving the problem stated above.)',
+        name: 'functionalityHighlight1'
+    },
+    {
+        type: 'input',
+        message: 'What is the second functionality you would like to highligh? (it should show how you are solving the problem stated above.)',
+        name: 'functionalityHighlight2'
+    },
+    {
+        type: 'input',
+        message: 'Who or what organization would you like to get credit to for their assistance in solving your problem?',
+        name: 'credit1'
+    },
+    {
+        type: 'list',
+        message: 'What pronoun would you like to use when referring to this person/organization? (use arrow keys to make a choice)',
+        choices: ['his', 'her', 'their'],
+        name: 'pronoun1'
+    },
+    {
+        type: 'input',
+        message: 'Please input a few words to complete this sentence, to describe the way they helped:...I would like to thank Steve for his .....',
+        name: 'helpWhy1'
+    },
+    {
+        type: 'input',
+        message: 'Please input a link for that person/organization so that your reader can access their portfolio or home page',
+        name: 'helpLink1'
+    },
+    {
+        type: 'input',
+        message: 'Who or what organization would you like to get credit to for their assistance in solving your problem?',
+        name: 'credit2'
+    },
+    {
+        type: 'list',
+        message: 'What pronoun would you like to use when referring to this person/organization? (use arrow keys to make a choice)',
+        choices: ['his', 'her', 'their'],
+        name: 'pronoun2'
+    },
+    {
+        type: 'input',
+        message: 'Please input a few words to complete this sentence, to describe the way they helped:...I would like to thank Steve for his .....',
+        name: 'helpWhy2'
+    },
+    {
+        type: 'input',
+        message: 'Please input a link for that person/organization so that your reader can access their portfolio or home page',
+        name: 'helpLink2'
+    },
+    {
+        type: 'input',
+        message: 'Who or what organization would you like to get credit to for their assistance in solving your problem?',
+        name: 'credit3'
+    },
+    {
+        type: 'list',
+        message: 'What pronoun would you like to use when referring to this person/organization? (use arrow keys to make a choice)',
+        choices: ['his', 'her', 'their'],
+        name: 'pronoun3'
+    },
+    {
+        type: 'input',
+        message: 'Please input a few words to complete this sentence, to describe the way they helped:...I would like to thank Steve for his .....',
+        name: 'helpWhy3'
+    },
+    {
+        type: 'input',
+        message: 'Please input a link for that person/organization so that your reader can access their portfolio or home page',
+        name: 'helpLink3'
+    },
+    {
+        type: 'list',
+        message: 'Please choose from the following: (use arrow to choose response)',
+        choices: ['MIT', 'Apache', 'GNU GPLv3'],
+        name: 'license'
+    }
+]
+
+function promptInquirer (inquiry){
+inquirer.prompt(inquiry).then( (answers) => {
+    console.log(answers)
+    console.log(answers.title)
+
+
+    let format = 
+    `#${answers.title}
+
+
+    (https://img.shields.io/github/last-commit/${answers.userName}/${answers.repoName}?style=plastic)
+    
+    This is an application that ${answers.shortDescription}. The purpose of this application is to solve ${answers.problem}. 
+    
+    ###Demo 
+    (${answers.screenshot})
+    [${answers.title}](${answers.liveLink})
+    ##Table of Contents
+        
+            *[Technologies](#technologies)
+            *[Installation](#installation)
+            *[Usage](#usage)
+            *[Credits](#credits)
+            *[Contributing](#contributing)
+            *[License](#license)
+    
+    ###Technologies
+    
+  
+    The technologies utilized in this application are as follows: ${answers.npmPackage}, ${answers.technology2}, and ${answers.technology3}. 
+        ${answers.npmPackage} was used because ${answers.tech1Why}. (https://img.shields.io/npm/l/${answers.npmPackage}?style=plastic)
+        ${answers.technology2} was used because ${answers.tech2Why}. 
+        ${answers.technology3} was used because ${answers.tech3Why}. 
+    
+    ###Installation
+    
+        To run this application successfully follow these steps:
+        1-Install ${answers.repositoryLink} to your local drive vis terminal/GitBash. This will install the package.json file which contains all the dependencies for this application. 
+        2-Verify that node is installed in your computer by typing 'node' in the terminal/GitBash. If it returns
+                Welcome to Node.js v12.14.1. (or higher)
+                Type ".help" for more information.
+            Node.js is installed in your computer. If nothing happens or there is an error, visit [Node.js](https://nodejs.org/) and install the LTS version.
+        3-
+    
+    ###Usage
+    
+        The purpose of this program is to solve ${answers.problem}, as mentioned above. Let's look a little further at how that can be accomplished.
+        *${answers.functionalityHighlight1} -  ${answers.functionalityHighlightHow1}
+        *${answers.functionalityHighlight2} -  ${answers.functionalityHighlightHow2}
+    
+    ###Credits
+    
+        The Dev community prides itself in the open source culture that it celebrates and maintains. This application couldn't have been made possible without the help of
+        *${answers.credit1} for ${answers.pronoun1} ${answers.helpwhy1}. ${answers.helpLink1}
+        *${answers.credit2} for ${answers.pronoun2} ${answers.helpwhy2}. ${answers.helpLink2}
+        *${answers.credit3} for ${answers.pronoun3} ${answers.helpwhy3}. ${answers.helpLink3} 
+    
+        ####Thank You!
+    
+    ###Contributing
+    
+    ###License
+            Licensed under the [${answers.license}](license) license. (https://img.shields.io/github/license/${answers.userName}/${answers.repoName}?style=plastic)
+    `
+
+    writeToFile(format)
+}
+
+)}
+promptInquirer(questions);
+
+
+function writeToFile ( data ){
+    fs.writeFile('README.md', data , function (err) {
+        if (err) throw err;
+        console.log('file made')
+    })
+}
 
