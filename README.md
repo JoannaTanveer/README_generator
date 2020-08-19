58a7c81ba9d75b7345b05b5bc0eddf3a0f566f22
JoannaTanveer <joanna.tanveer@gmail.com>
2020-08-18 21:47:48 -0700

    format correct, not to make it more complicated

--- README.md
+++ README.md
@@ -1,56 +1,56 @@
-# README generator
+
+# README Generator
 
 
-    (https://img.shields.io/github/last-commit/JoannaTanveer/README_generator?style=plastic)
+(https://img.shields.io/github/last-commit/JoannaTanveer/https://joannatanveer.github.io/README_generator/?style=plastic)
     
-    This is an application that This is a node.js application that will Prompt the user to answer a series of questions which are then dynamically added to a template. Immediately following a README.md file is generated in the application folder and is ready to commit.. The purpose of this application is to solve Developers spend months on an application, but only an hour on their readme. This application creates a quality README.md file without all the headaches of formating and styling.. 
+This is an application that js. The purpose of this application is to solve pdjh. 
     
-    ### Demo 
-    (null)
+### Demo 
+(sjhd)
     
-    ## Table of Contents
+## Table of Contents
         
-    - [Technologies](#technologies)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [Contributing](#contributing)
-    - [License](#license)
+- [Technologies](#technologies)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [License](#license)
     
-    ### Technologies
+### Technologies
     
-    The technologies utilized in this application are as follows: inquirer, screencastify, and joij. 
-    inquirer was used because questions and answers. (https://img.shields.io/npm/l/inquirer?style=plastic)
-    screencastify was used because videa. 
-    joij was used because kjlkjlkj. 
+The technologies utilized in this application are as follows: f;hljk, ;, and ;dlfh. 
+- f;hljk was used because sjh. (https://img.shields.io/npm/l/f;hljk?style=plastic)
+- ; was used because js. 
+- ;dlfh was used because sj. 
     
-    ### Installation
+### Installation
     
-    To run this application successfully follow these steps:
-    1. Install undefined to your local drive vis terminal/GitBash. This will install the package.json file which contains all the dependencies for this application. 
-    2. Verify that node is installed in your computer by typing 'node' in the terminal/GitBash. If it returns
-        >Welcome to Node.js v12.14.1. (or higher)
-        >Type ".help" for more information.
+To run this application successfully follow these steps:
+1. Install undefined to your local drive vis terminal/GitBash. This will install the package.json file which contains all the dependencies for this application. 
+2. Verify that node is installed in your computer by typing 'node' in the terminal/GitBash. If it returns
+     >Welcome to Node.js v12.14.1. (or higher)
+    >Type ".help" for more information.
     
-       Node.js is installed in your computer. If nothing happens or there is an error, visit [Node.js](https://nodejs.org/) and install the LTS version.
+ Node.js is installed in your computer. If nothing happens or there is an error, visit [Node.js](https://nodejs.org/) and install the LTS version.
     
-    ### Usage
+### Usage
     
-    The purpose of this program is to solve Developers spend months on an application, but only an hour on their readme. This application creates a quality README.md file without all the headaches of formating and styling., as mentioned above. Let's look a little further at how that can be accomplished.
-    - flfgkj;alksjdg because kgf;lkd
-    - flkdasfjh;lkdjs becasue
+The purpose of this program is to solve pdjh, as mentioned above. Let's look a little further at how that can be accomplished.
+- h;
+- sjhl;
     
-    ### Credits
+### Credits
     
-    The Dev community prides itself in the open source culture that it celebrates and maintains. This application couldn't have been made possible without the help of
-    - kfgsdkgj for his undefined. https://github.com/anthonychry
-    - fglis;doiguj for their undefined. https://www.linkedin.com/in/brian-duimstra-61878525/
-    - rgfsrga for it's undefined. gjrg;lkj 
+The Dev community prides itself in the open source culture that it celebrates and maintains. This application couldn't have been made possible without the help of
+- sjdf for his undefined. shklg'd
+- shjk'dl for it's undefined. sh'kldhf
+- s'lkhg for his undefined. shkl'hd 
     
-        #### Thank You!
+#### Thank You!
     
-    ### Contributing
+### Contributing
     
-    ### License
-    Licensed under the [MIT] license. (https://img.shields.io/github/license/JoannaTanveer/README_generator?style=plastic)
-    
+### License
+Licensed under the [GNU GPLv3] license. (https://img.shields.io/github/license/JoannaTanveer/https://joannatanveer.github.io/README_generator/?style=plastic)
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const format = require('format.md')
+// const format = require('format.md')
 const questions =  [
     {
      type: 'input',
@@ -161,62 +161,64 @@ inquirer.prompt(inquiry).then( (answers) => {
 
 
     let format = 
-    `# ${answers.title}
+`
+# ${answers.title}
 
 
-    (https://img.shields.io/github/last-commit/${answers.userName}/${answers.repoName}?style=plastic)
+![Badge](https://img.shields.io/github/last-commit/${answers.userName}/${answers.repoName}?style=plastic)
     
-    This is an application that ${answers.shortDescription}. The purpose of this application is to solve ${answers.problem}. 
+This is an application that ${answers.shortDescription}. The purpose of this application is to solve ${answers.problem}. 
     
-    ### Demo 
-    (${answers.screenshot})
+### Demo 
+![image relative path](${answers.screenshot})
     
-    ## Table of Contents
+## Table of Contents
         
-    - [Technologies](#technologies)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [Contributing](#contributing)
-    - [License](#license)
+- [Technologies](#technologies)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [License](#license)
     
-    ### Technologies
+### Technologies
     
-    The technologies utilized in this application are as follows: ${answers.npmPackage}, ${answers.technology2}, and ${answers.technology3}. 
-    ${answers.npmPackage} was used because ${answers.tech1Why}. (https://img.shields.io/npm/l/${answers.npmPackage}?style=plastic)
-    ${answers.technology2} was used because ${answers.tech2Why}. 
-    ${answers.technology3} was used because ${answers.tech3Why}. 
+The technologies utilized in this application are as follows: ${answers.npmPackage}, ${answers.technology2}, and ${answers.technology3}. 
+- ${answers.npmPackage} was used because ${answers.tech1Why}.![link] (https://img.shields.io/npm/l/${answers.npmPackage}?style=plastic)
+- ${answers.technology2} was used because ${answers.tech2Why}. 
+- ${answers.technology3} was used because ${answers.tech3Why}. 
     
-    ### Installation
+### Installation
     
-    To run this application successfully follow these steps:
-    1. Install ${answers.repositoryLink} to your local drive vis terminal/GitBash. This will install the package.json file which contains all the dependencies for this application. 
-    2. Verify that node is installed in your computer by typing 'node' in the terminal/GitBash. If it returns
-        >Welcome to Node.js v12.14.1. (or higher)
-        >Type ".help" for more information.
+To run this application successfully follow these steps:
+1. Install ![Link](${answers.repositoryLink}) to your local drive vis terminal/GitBash. This will install the package.json file which contains all the dependencies for this application. 
+2. Verify that node is installed in your computer by typing 'node' in the terminal/GitBash. If it returns
+    >Welcome to Node.js v12.14.1. (or higher)
+    >
+    >Type ".help" for more information.
     
-       Node.js is installed in your computer. If nothing happens or there is an error, visit [Node.js](https://nodejs.org/) and install the LTS version.
+ Node.js is installed in your computer. If nothing happens or there is an error, visit [Node.js](https://nodejs.org/) and install the LTS version.
     
-    ### Usage
+### Usage
     
-    The purpose of this program is to solve ${answers.problem}, as mentioned above. Let's look a little further at how that can be accomplished.
-    - ${answers.functionalityHighlight1}
-    - ${answers.functionalityHighlight2}
+The purpose of this program is to solve ${answers.problem}, as mentioned above. Let's look a little further at how that can be accomplished.
+- ${answers.functionalityHighlight1}
+- ${answers.functionalityHighlight2}
     
-    ### Credits
+### Credits
     
-    The Dev community prides itself in the open source culture that it celebrates and maintains. This application couldn't have been made possible without the help of
-    - ${answers.credit1} for ${answers.pronoun1} ${answers.helpwhy1}. ${answers.helpLink1}
-    - ${answers.credit2} for ${answers.pronoun2} ${answers.helpwhy2}. ${answers.helpLink2}
-    - ${answers.credit3} for ${answers.pronoun3} ${answers.helpwhy3}. ${answers.helpLink3} 
+The Dev community prides itself in the open source culture that it celebrates and maintains. This application couldn't have been made possible without the help of
+- ${answers.credit1} for ${answers.pronoun1} ${answers.helpwhy1}. ![link](${answers.helpLink1})
+- ${answers.credit2} for ${answers.pronoun2} ${answers.helpwhy2}. ![link](${answers.helpLink2})
+- ${answers.credit3} for ${answers.pronoun3} ${answers.helpwhy3}. ![link](${answers.helpLink3})
     
-        #### Thank You!
+#### Thank You!
     
-    ### Contributing
+### Contributing
     
-    ### License
-    Licensed under the [${answers.license}] license. (https://img.shields.io/github/license/${answers.userName}/${answers.repoName}?style=plastic)
-    `
+### License
+Licensed under the ${answers.license} license. ![link](https://img.shields.io/github/license/${answers.userName}/${answers.repoName}?style=plastic)
+`
 
     writeToFile(format)
 }
